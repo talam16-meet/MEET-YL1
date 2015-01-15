6f233d3cc66eae8a754a0df33f65bfe75b61cef1
Tala Manna <talam16@meet.mit.edu>
2015-01-15 20:09:59 +0200

    oop

---
+++ oop.py
@@ -0,0 +1,12 @@
+class Animal:
+	def __ init__ (self, name, age, color, size):
+		self.name=name		
+		self.age=age
+		self.color=color
+		self.size=size
+
+dog=Animal("Nour Awwad", 2, "Black", "overweight")
+print (dog.name)
+print (dog.age)
+print (dog.color)
+print (dog.size)
